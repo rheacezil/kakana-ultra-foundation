364ec9c2e43287909b817ac8e66dae691f370449
rheacezil <57025850+rheacezil@users.noreply.github.com>
2022-09-05 10:31:03 +0800

    updated initial setup and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NavigationBar from './components/NavigationBar';
+import Home from './components/pages/Home';
+import LandingPage from './components/pages/LandingPage';
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
 
 function App() {
   return (
     <div className="App">
-     <h1>hello</h1>
+     <BrowserRouter>
+     {/* Navigation bar should be a ternary operator of whether or not a user is logged in. */}
+     <NavigationBar />
+     <Routes>
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/home' element={<Home />} />
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
